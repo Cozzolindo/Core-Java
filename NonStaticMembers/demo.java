e1f769d9399634a11dd62e709ba450a8c2ba071f
Carlos Cozzolino <cozzolinocarlos@gmail.com>
2022-03-15 19:28:39 -0300

    Create References in a Static Method

--- NonStaticMembers/demo.java
+++ NonStaticMembers/demo.java
@@ -3,13 +3,18 @@ package NonStaticMembers;
 public class demo {
 
     static demo obj;
+
     static{
         System.out.println(demo.obj);
-        demo.obj = new demo();
+        demo.obj = demo.init();
         
     }
 
     public static void main(String[] args) {
         System.out.println(demo.obj);
+    }
+
+    static demo init(){
+        return new demo();
     }
 }
