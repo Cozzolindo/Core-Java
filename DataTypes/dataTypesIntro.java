121e830535a2df3196a68fac342bef00a33429b5
Carlos Cozzolino <cozzolinocarlos@gmail.com>
2022-03-15 19:47:00 -0300

    Data Types Intro

---
+++ DataTypes/dataTypesIntro.java
@@ -0,0 +1,14 @@
+package DataTypes;
+
+public class dataTypesIntro {
+    
+    byte a = 120;
+    short b = 150;
+    int c = 10000;
+    long d = 1000000;
+    char e = 'e';
+    float f = 1.23f;
+    double g = 1.25;
+    byte h = 20;
+    int result = h+a;
+}
