0f76efb10cd5a4d17850d09d5625641f1aa057e3
Carlos Cozzolino <cozzolinocarlos@gmail.com>
2022-03-15 20:05:35 -0300

    Explicit Casting

---
+++ DataTypes/explicitCasting.java
@@ -0,0 +1,15 @@
+package DataTypes;
+
+public class explicitCasting {
+    
+    public static void main(String[] args) {
+        int i = 100;
+        byte b = (byte)i;
+
+        int k = 97;
+        char ch = (char)k;
+
+        System.out.println(b);
+        System.out.println(ch);
+    }
+}
