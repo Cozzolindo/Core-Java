42bb3604068a5ff3b655eb302c6ef1ee2dd88c63
Carlos Cozzolino <cozzolinocarlos@gmail.com>
2022-03-15 17:31:18 -0300

    Static Variables

---
+++ JavaBasics/staticVariables.java
@@ -0,0 +1,11 @@
+package JavaBasics;
+
+public class staticVariables {
+
+    static double num = 5.1;
+    static int numb;
+    public static void main(String[] args) {
+        System.out.println(num);
+        System.out.println(numb);
+    }
+}
