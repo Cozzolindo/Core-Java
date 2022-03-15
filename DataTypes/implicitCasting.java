1218a8e92b3b763186c4868f85924823225c10d6
Carlos Cozzolino <cozzolinocarlos@gmail.com>
2022-03-15 20:07:28 -0300

    Implicit Casting

--- DataTypes/implicitCasting.java
+++ DataTypes/implicitCasting.java
@@ -1,6 +1,6 @@
 package DataTypes;
 
-import javax.swing.event.SwingPropertyChangeSupport;
+
 
 public class implicitCasting {
     public static byte b = 100;
@@ -8,8 +8,11 @@ public class implicitCasting {
     static long l = i;
     static char ch = 'A';
     static int x = ch;
+
+    static byte result = (byte)(b+i);
     public static void main(String[] args) {
         System.out.println(i);
         System.out.println(x);
+        System.out.println(result);
     }
 }
