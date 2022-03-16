912110e24ffc4d553a0792377af8f3363abb5fa6
Carlos Cozzolino <cozzolinocarlos@gmail.com>
2022-03-16 18:46:38 -0300

    Increment and Decrement

---
+++ Operators/unaryOperators.java
@@ -0,0 +1,13 @@
+package Operators;
+
+public class unaryOperators {
+    
+    public static void main(String[] args) {
+        int x = 10;
+        int y = x++;    
+
+        System.out.println(x);
+        System.out.println("Value of Y :");
+        System.out.println(y);
+    }
+}
