669c457562b9345bf0f4051e28353ef5529703d5
Carlos Cozzolino <cozzolinocarlos@gmail.com>
2022-03-16 16:19:52 -0300

    Primitive and Strings

---
+++ DataTypes/primitiveAndStrings.java
@@ -0,0 +1,10 @@
+package DataTypes;
+
+import javax.swing.plaf.basic.BasicSplitPaneUI.BasicHorizontalLayoutManager;
+
+public class primitiveAndStrings {
+    byte x = 100;
+
+    String s = Byte.toString(x);
+    byte y = Byte.parseByte(s);
+}
