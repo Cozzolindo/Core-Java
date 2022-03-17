e78a29c2df8a7db0ec72a7c7989239148de22f6b
Carlos Cozzolino <cozzolinocarlos@gmail.com>
2022-03-17 16:39:22 -0300

    Ternary operators

---
+++ Operators/ternaryOperators.java
@@ -0,0 +1,11 @@
+package Operators;
+
+public class ternaryOperators {
+    
+    public static void main(String[] args) {
+        int x = 20;
+        int y = 30;
+        String result = (x>y)?"X is greater":"Y is greater";
+        System.out.println(result);
+    }
+}
