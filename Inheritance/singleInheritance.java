87d749f33d4efc76e3cf27e7395743a97b682a89
Carlos Cozzolino <cozzolinocarlos@gmail.com>
2022-03-21 16:33:25 -0300

    Single Inheritance

---
+++ Inheritance/singleInheritance.java
@@ -0,0 +1,15 @@
+package Inheritance;
+
+public class singleInheritance {
+    
+    void m1(){
+        System.out.println("Inside m1");
+    }
+    public static void main(String[] args) {
+        singleInheritance s = new singleInheritance();
+        s.m1();
+        s.hashCode();
+        s.getClass();
+    }
+    
+}
