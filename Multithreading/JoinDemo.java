787bd5656792ae67e444b620827ecbd0e3826eea
Carlos Cozzolino <cozzolinocarlos@gmail.com>
2022-03-24 17:18:58 -0300

    Thread Identity

--- Multithreading/JoinDemo.java
+++ Multithreading/JoinDemo.java
@@ -11,20 +11,28 @@ public class JoinDemo extends Thread{
         System.out.println("Enter a value: ");
         Scanner scan = new Scanner(System.in);
         JoinDemo.n = scan.nextInt();
-        
+        Thread ct = Thread.currentThread();
+
+        long start = System.currentTimeMillis();
         JoinDemo jd = new JoinDemo();
         jd.start();
         try {
             jd.join();
+            System.out.println("Current thread is: "+ct.getName());
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
         System.out.println("Factorial of " + JoinDemo.n + " is " + JoinDemo.sum);
+        long end = System.currentTimeMillis();
+
+        System.out.println((end-start) + "milisseconds elapsed.");
     }
 
     public void run(){
+        Thread ct = Thread.currentThread();
         for(int i = 1; i<=JoinDemo.n; i++){
+            System.out.println("Current thread is: "+ct.getName());
             JoinDemo.sum *= i;
         }
     }
