e0c7a4563c19357805991dc23669a022c7682900
Carlos Cozzolino <cozzolinocarlos@gmail.com>
2022-03-30 16:41:40 -0300

    Acessing Outer Classes members

--- InnerClasses/NonStatic/Outer.java
+++ InnerClasses/NonStatic/Outer.java
@@ -2,10 +2,22 @@ package InnerClasses.NonStatic;
 
 public class Outer {
     
+    private static int x = 50;
+    private int y;
 
+    Outer(int y){
+        this.y = y;
+    }
     class Inner{
+
+        private int y;
+        Inner(int y){
+            this.y = y;
+        }
         void f1(){
-            System.out.println("Non static Inner Method");
+            System.out.println("Outer.x " + Outer.x);
+            System.out.println("Outer.this.y "+Outer.this.y);
+            System.out.println("this.y "+this.y);
         }
     }
 
@@ -15,10 +27,11 @@ public class Outer {
 
     public static void main(String[] args) {
         
-        Outer fora = new Outer();
+        Outer fora = new Outer(50);
         fora.f2();
 
-        Outer.Inner inner = fora.new Inner();
+        Outer.Inner inner = fora.new Inner(30);
         inner.f1();
+        System.out.println(inner.y);
     }
 }
