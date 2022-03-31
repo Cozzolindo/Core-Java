537e383396d39096595c604cafc27a72e9e1fc45
Carlos Cozzolino <cozzolinocarlos@gmail.com>
2022-03-31 15:58:15 -0300

    String Intro

---
+++ Strings/StringDemo.java
@@ -0,0 +1,14 @@
+package Strings;
+
+public class StringDemo {
+    
+    public static void main(String[] args) {
+        
+        String s1 = "Nath";
+        String s2 = new String("acha");
+        char n[] = {'n','k'};
+        String s3 = new String(n);
+        
+        System.out.println(s1+s2+s3);
+    }
+}
