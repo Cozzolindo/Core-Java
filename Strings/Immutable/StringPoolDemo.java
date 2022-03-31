67b42444437cad291b570e51ba7068714ca27f1d
Carlos Cozzolino <cozzolinocarlos@gmail.com>
2022-03-31 16:59:33 -0300

    String Comparison

--- Strings/Immutable/StringPoolDemo.java
+++ Strings/Immutable/StringPoolDemo.java
@@ -12,10 +12,13 @@ public class StringPoolDemo {
 
         String s1 = "abc";
         String s2 = "abc";
+        String s3 = new String("abc");
         System.out.println(s1.hashCode());
         System.out.println(s2.hashCode());
 
-        System.out.println(s1 == s2);
-        System.out.println(s1.equals(s2));
+        System.out.println(s1 == s2); // Compare memory location
+        System.out.println(s1.equals(s2)); // Compare content of String
+        System.out.println(s1.equals(s3));
+        System.out.println(s1 == s3);
 ;    }
 }
