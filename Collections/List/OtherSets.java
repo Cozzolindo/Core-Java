54fa54840ddbbb423d6e3d1da486632b262760a7
Carlos Cozzolino <cozzolinocarlos@gmail.com>
2022-04-27 22:03:09 -0300

    Different Set Classes

---
+++ Collections/List/OtherSets.java
@@ -0,0 +1,31 @@
+package Collections.List;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Random;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class OtherSets {
+    
+    public static void main(String[] args) {
+        
+        Random obj = new Random();
+
+        //HashSet<Integer> sett = new HashSet<>(); // Does not maintain it`s order
+
+        //LinkedHashSet<Integer> sett = new LinkedHashSet<>(); // Maintain it's order
+
+        Set<Integer> sett = new TreeSet<>();
+        for(int i = 0; i<10; i++){
+            int num = obj.nextInt(100);
+            sett.add(num);
+            System.out.print(num);
+            System.out.print(" ");
+        }
+
+        System.out.println();
+        System.out.println("Set: "+sett);
+
+    }
+}
