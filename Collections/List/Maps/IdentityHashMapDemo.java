efe420359ddee6e7b2f28b7e6137e4111015e4ac
Carlos Cozzolino <cozzolinocarlos@gmail.com>
2022-05-02 17:24:27 -0300

    IdentityHashMap Intro

---
+++ Collections/List/Maps/IdentityHashMapDemo.java
@@ -0,0 +1,20 @@
+package Collections.List.Maps;
+
+import java.util.HashMap;
+import java.util.IdentityHashMap;
+import java.util.Map;
+
+public class IdentityHashMapDemo {
+    
+    public static void main(String[] args) {
+        
+        Map<Integer, String> map = new IdentityHashMap<>();
+        Integer id1 = new Integer(10);
+        Integer id2 = new Integer(10);
+        
+        map.put(id1, "JoJo");
+        map.put(id2, "FMAB");
+
+        System.out.println(map);
+    }
+}
