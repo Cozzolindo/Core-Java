a6c5e82b6004402affb8dcf00676052527271723
Carlos Cozzolino <cozzolinocarlos@gmail.com>
2022-05-02 17:07:14 -0300

    HashSet Intro

---
+++ Collections/List/HashMapDemo.java
@@ -0,0 +1,30 @@
+package Collections.List;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class HashMapDemo {
+ 
+    public static void main(String[] args) {
+        
+        Map<String,Integer> map = new HashMap<>();
+
+        map.put("Carlos", 26);
+        map.put("Luis", 24);
+        map.put("Carol", 26);
+        map.put("Kattya", 22);
+
+        Set<String> keySet = map.keySet();
+        System.out.println("Keys" + keySet);
+
+        Collection<Integer> values = map.values();
+        System.out.println("Values" + values);
+
+        for (String key : keySet) {
+            System.out.println("Key: "+key+"\nValue: "+map.get(key));
+        }
+
+    }
+}
