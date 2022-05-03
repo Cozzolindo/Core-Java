4f7aa515de4642d0946be9cdcd55c1f766ee3489
Carlos Cozzolino <cozzolinocarlos@gmail.com>
2022-05-03 16:21:31 -0300

    Weak HashMap

---
+++ Collections/List/Maps/User.java
@@ -0,0 +1,15 @@
+package Collections.List.Maps;
+
+public class User {
+    
+    @Override
+    public String toString() {
+        
+        return "User";
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        System.out.println("Finalize Called");
+    }
+}
