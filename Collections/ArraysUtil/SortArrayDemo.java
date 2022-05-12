8183347ba0c98af24f88df0600c4f40a19c7adac
Carlos Cozzolino <cozzolinocarlos@gmail.com>
2022-05-12 11:35:59 -0300

    Custom Comparator for Arrays

--- Collections/ArraysUtil/SortArrayDemo.java
+++ Collections/ArraysUtil/SortArrayDemo.java
@@ -2,6 +2,8 @@ package Collections.ArraysUtil;
 
 import java.util.Arrays;
 
+import Collections.Comparator.MyComparator;
+
 public class SortArrayDemo {
     
     public static void main(String[] args) {
@@ -15,6 +17,13 @@ public class SortArrayDemo {
         System.out.println("After Sorting: ");
         for (int eachElem : a) {
             System.out.println(eachElem);
+        }
+
+        String[] s = {"B","C","Z","A"};
+        System.out.println("After sorting:");
+        Arrays.sort(s, new MyComparator());
+        for (String eachString : s) {
+            System.out.println(eachString);
         }
     }
 }
