f51a7b1c38c81540a6c31f060c8936a9cd6aa07d
Carlos Cozzolino <cozzolinocarlos@gmail.com>
2022-05-12 10:55:57 -0300

    My Comparator

---
+++ Collections/CollectionsUtil/SortedList.java
@@ -0,0 +1,27 @@
+package Collections.CollectionsUtil;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import Collections.Comparator.*;
+
+public class SortedList {
+    
+    public static void main(String[] args) {
+        
+        List<String> list = new ArrayList<>();
+        
+        list.add("Z");
+        list.add("A");
+        list.add("C");
+        list.add("K");
+
+        System.out.println("Before Sorting: "+ list);
+
+        Collections.sort(list, new MyComparator());
+
+        System.out.println("After Sorting: "+ list);
+
+        Collections.sort(list, new MyComparator());
+    }
+}
