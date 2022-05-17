ed17f2cd5b897af925f5304fc64054d33310aae8
Carlos Cozzolino <cozzolinocarlos@gmail.com>
2022-05-17 11:33:13 -0300

    Generics

---
+++ Collections/Generics/TypeCastingProblem.java
@@ -0,0 +1,22 @@
+package Collections.Generics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TypeCastingProblem {
+    public static void main(String[] args) {
+        String[] s = new String[10];
+        s[0] = "Carlos";
+        s[1] = "Cozzolino";
+        //s[2] = 123; -> Error
+
+        List<String> l = new ArrayList<>();
+        l.add("Caro");
+        l.add("Line");
+        l.add(26);
+
+        String name = (String)l.get(0);
+        System.out.println(name);
+
+    }
+}
