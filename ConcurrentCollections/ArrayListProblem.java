7f77ffc863dbb7847977349542f0f83b447a09d0
Carlos Cozzolino <cozzolinocarlos@gmail.com>
2022-06-07 18:00:47 -0300

    Using Multiple Threads

--- ConcurrentCollections/ArrayListProblem.java
+++ ConcurrentCollections/ArrayListProblem.java
@@ -4,25 +4,39 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.concurrent.CopyOnWriteArrayList;
 
-public class ArrayListProblem {
+public class ArrayListProblem extends Thread {
     
-    public static void main(String[] args) {
-        
-        CopyOnWriteArrayList<String> courses = new CopyOnWriteArrayList<String>();
+    static CopyOnWriteArrayList<String> courses = new CopyOnWriteArrayList<String>();
 
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
         courses.add("Java");
-        courses.add("Python");
+        
         courses.add("C");
         courses.add("C#");
+        
+        
+    }
+    public static void main(String[] args) throws InterruptedException {
+        
+        ArrayListProblem arrayListProblem = new ArrayListProblem();
+        arrayListProblem.start();
+
+        courses.add("Python");
+        
 
         Iterator<String> iterator = courses.iterator();
 
         while(iterator.hasNext()){
+            Thread.sleep(1000);
             String course = iterator.next();
-            
-            if(course.equals("C")){
-                courses.remove(course);
-            }
+            System.out.println(course);
         }
 
         System.out.println(courses);
