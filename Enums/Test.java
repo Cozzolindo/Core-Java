584d69101a5527acc43e953defe841c828a85ba4
Carlos Cozzolino <cozzolinocarlos@gmail.com>
2022-06-09 15:44:12 -0300

    Using Enums

---
+++ Enums/Test.java
@@ -0,0 +1,10 @@
+package Enums;
+
+public class Test {
+    
+    public static void main(String[] args) {
+        
+        PaymentEnum pt = PaymentEnum.CASH;
+        System.out.println(pt);
+    }
+}
