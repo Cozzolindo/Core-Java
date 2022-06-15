c59b0087f3961b3632d490726c5aa4f14149923b
Carlos Cozzolino <cozzolinocarlos@gmail.com>
2022-06-15 17:16:54 -0300

    Using Mapping

---
+++ Java8/MethodRef/Filters/UpperToLower.java
@@ -0,0 +1,19 @@
+package Java8.MethodRef.Filters;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class UpperToLower {
+    
+    public static void main(String[] args) {
+        
+        List<String> list = new ArrayList<>();
+        list.add("Guilherme");
+        list.add("Ana");
+        list.add("Beatriz");
+
+        List<String> collect = list.stream().map(s->s.toUpperCase()).collect(Collectors.toList());
+        System.out.println(collect);
+    }
+}
