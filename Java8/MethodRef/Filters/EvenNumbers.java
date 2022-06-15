1c24101e34b7616dee3a6bbdecc46ae084c7e982
Carlos Cozzolino <cozzolinocarlos@gmail.com>
2022-06-15 17:02:05 -0300

    Using Filters

---
+++ Java8/MethodRef/Filters/EvenNumbers.java
@@ -0,0 +1,30 @@
+package Java8.MethodRef.Filters;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+public class EvenNumbers {
+    
+    public static void main(String[] args) {
+
+        Random rand = new Random();
+        List<Integer> list = new ArrayList<Integer>();
+        for(int i =0; i<=10;i++){
+            list.add(rand.nextInt(20));
+        }
+        for (Integer integer : list) {
+            
+            if(integer%2==0){
+                System.out.println(integer + " it's even!");
+            }else{
+                System.out.println(integer);
+            }
+        }
+
+        List<Integer> collect = list.stream().filter(i->i%2==0).collect(Collectors.toList());
+        System.out.println(collect);
+    }
+}
