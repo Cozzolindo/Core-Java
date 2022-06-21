99af631aed6e91b5010e3a4535b7b2797c9138a6
Carlos Cozzolino <cozzolinocarlos@gmail.com>
2022-06-21 19:36:20 -0300

    Locale

---
+++ Internationalization/LocaleTest.java
@@ -0,0 +1,15 @@
+package Internationalization;
+
+import java.util.Locale;
+
+public class LocaleTest {
+    
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Locale l = Locale.getDefault();
+
+        
+        System.out.println(l.getCountry() + " " + l.getLanguage());
+        System.out.println(l.getDisplayCountry() + " " + l.getDisplayLanguage());
+    }
+}
